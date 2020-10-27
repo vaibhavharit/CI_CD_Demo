1da3920bc7dc3ad54d5769ce856f19de2b592b2c
Vaibhav Harit <vaibhav16x@gmail.com>
2020-10-27 20:25:10 +0530

    Project added

---
+++ src/main/java/com/example/demo/resources/WelcomeRestController.java
@@ -0,0 +1,25 @@
+package com.example.demo.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@Api(value = "Swagger2WelcomeRestController", description = "This REST Api related to Welcome Message!!!!")
+@RestController
+public class WelcomeRestController {
+
+	@ApiOperation(value = "Get Welcome Message For The Given Name ", response = String.class, tags = "getWelcomeNote")
+	@GetMapping("/welcome/{name}")
+	public String welcome(@PathVariable("name") String name) {
+		return "Welcome to Sporty Shoes !" + name;
+	}
+		
+	@GetMapping("/admins")
+	public String returnAdmin() {
+		return "Name: Deepak. Contact: deepak.kumar@prolim.com" ;
+	}
+	
+}	
